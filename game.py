cf100fe10b25874a515c100da3009c8d48f1cdae
Wendy Wang <wendyhtwang@uchicago.edu>
2024-08-30 15:11:05 -0500

    part 5 done!

--- game.py
+++ game.py
@@ -27,8 +27,8 @@ def main():
     guessed = set()
 
     # pick a word at random
-    #word = random.choice(words)
-    word = words[0]  # temporarily just use the first word 
+    word = random.choice(words)
+    #word = words[0]  # temporarily just use the first word 
 
     # play the game until they win or run out of guesses
     while guesses > 0:
